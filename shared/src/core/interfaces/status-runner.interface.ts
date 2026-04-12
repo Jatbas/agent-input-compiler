ce1bee2f8765c877d0a36024d8e88a44c374e5cc
Jorge Santos <jorge.santos@valegachain.com>
2026-04-12 23:34:16 +0300

    refactor(core): remove unused StatusRunner port

--- shared/src/core/interfaces/status-runner.interface.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright (c) 2025 AIC Contributors
-
-import type { StatusRequest } from "@jatbas/aic-core/core/types/status-types.js";
-import type { StatusAggregates } from "@jatbas/aic-core/core/types/status-types.js";
-
-export interface StatusRunner {
-  status(request: StatusRequest): Promise<StatusAggregates>;
-}
